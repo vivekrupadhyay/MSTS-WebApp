f95bdb8a0f59a5b3919797f113ba4c349114aea7
Vivekrupadhyay <viveku407@gmail.com>
2019-10-19 12:19:16 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,62 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { ImagemanagerComponent } from './Admin/imagemanager/imagemanager.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full', data: {
+      title: 'Home'
+    }
+  },
+  {
+    path: 'login', component: LoginComponent, data: {
+      title: 'Login'
+    }
+  },
+  // //{ path: 'login', loadChildren: './login/app.module#AppModuleModule' },
+  {
+    path: 'register', component: RegistrationComponent, data: {
+      title: 'User Registration'
+    }
+  },
+  // //{ path: 'register',loadChildren:'./registration/registration.module#registrationModule' },
+  {
+    path: 'contactus', component: ContactusComponent, data: {
+      title: 'Contact Us'
+    }
+  },
+
+  // { path: 'add-role', component: RoleComponent },
+  // { path: 'role-list', component: RoleListComponent },
+  {
+    path: 'home', component: HomeComponent, data: {
+      title: 'Home'
+    }
+  },
+  {
+    path: 'upload-banners', component:ImagemanagerComponent, data: {
+      title: 'Image Manager'
+    }
+  },
+
+  {
+    path: 'admin-dashboard',
+    component: AdminLayoutComponent,
+    children: [{
+      path: 'admin-dashboard',
+      loadChildren: './admin-layout/admin.module#AdminModule', data: {
+        title: 'Admin'
+      }
+    }]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { LoaderService } from './services/loader.service';
+import { TitleService } from './services/title.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MITSWebApp';
+export class AppComponent implements OnInit {
+  
+  constructor(public titleService: TitleService, public loaderService: LoaderService) { }
+  
+    
+  
+  ngOnInit() {
+    this.titleService.updateTitle();
+    this.loaderService.isLoading;
+  }
 }
